9ce94377e2a38774cdf921eade5f3061bface325
Leonardo Bezerra <leonardoalvesbezerra@gmail.com>
2026-01-03 05:58:13 -0300

    Add type command

--- app/main.py
+++ app/main.py
@@ -3,16 +3,28 @@ import sys
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
+    builtIns = ['echo', 'exit', 'type']
     while(1):
         sys.stdout.write("$ ")
 
         command = input()
+        command, _, command_content = command.partition(" ")
         if command == 'exit':
             break
-        elif command[:5] == 'echo ':
-            print(f"{command[5:]}")
+        elif command == 'echo':
+            print(f"{command_content}")
             continue
-
+        elif command == 'type':
+            found = False
+            for x in builtIns:
+                if x == command_content:
+                    print(f'{command_content} is a shell builtin')
+                    found = True
+                    break
+            if found:
+                continue 
+            else:
+                command = command_content       
 
         print(f"{command}: command not found")
 
