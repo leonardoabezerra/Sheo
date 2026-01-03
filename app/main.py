b948431b10d3e5b2e4df153c37ee56ec28310a83
Leonardo Bezerra <leonardoalvesbezerra@gmail.com>
2026-01-03 05:14:19 -0300

    Add while loop (REPL)

--- app/main.py
+++ app/main.py
@@ -3,10 +3,11 @@ import sys
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
-    sys.stdout.write("$ ")
+    while(1):
+        sys.stdout.write("$ ")
 
-    command = input()
-    print(f"{command}: command not found")
+        command = input()
+        print(f"{command}: command not found")
 
     pass
 
